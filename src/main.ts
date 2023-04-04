05c78cb84afe8e54a35729f31872ab9329d3052a
LYT <limyt0909>
2023-04-04 23:00:32 +0900

    es2017 to es2016

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,7 @@ const crawler = async () => {
   await app.listen(8080);
 }
 crawler();
+
+
+//https://arta1069.medium.com/nestjs-typeorm-graphql-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0-a1fe4e6fc823
+//해당문서 참조해서 익숙해지기 - 230404 용택
