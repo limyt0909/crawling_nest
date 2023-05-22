a0fdefba884b013191c9b98707d5d83cb65a1cc4
LYT <limyt0909>
2023-05-22 20:48:01 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 const crawler = async () => {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
@@ -11,4 +10,4 @@ crawler();
 
 
 //https://arta1069.medium.com/nestjs-typeorm-graphql-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0-a1fe4e6fc823
-//해당문서 참조해서 익숙해지기 - 230507 용택
+//해당문서 참조해서 익숙해지기 스터디 시작- 230522  용택
