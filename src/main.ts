4b990998c2ac01d541d13c2029f66ba7e512782c
LYT <limyt0909>
2023-05-23 21:14:41 +0900

    how to use Nest-js

--- src/main.ts
+++ src/main.ts
@@ -14,4 +14,14 @@ async function craw() {
 }
 craw()
 //https://arta1069.medium.com/nestjs-typeorm-graphql-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0-a1fe4e6fc823
-//해당문서 참조해서 익숙해지기 스터디 시작- 230522  용택
+//해당문서 참조해서 익숙해지기 스터디 시작- 230523  용택
+
+
+// Controllers: Define routes and handle requests.
+// Providers: Manage application logic and data.
+// Modules: Organize your application into reusable modules.
+// Dependency Injection: Easily manage and inject dependencies.
+// Middleware: Apply cross-cutting concerns to requests.
+// Pipes: Validate and transform request data.
+// Guards: Implement authorization and authentication logic.
+// Interceptors: Modify incoming/outgoing data in the request/response pipeline.
